347cf0dbbaf3a4ba8a8a295641d813923adc5f11
Dana Seong <mseo447@aucklanduni.ac.nz>
2023-08-20 20:18:50 +1200

    Added another component in the home page.

--- app/page.tsx
+++ app/page.tsx
@@ -69,6 +69,101 @@ export default async function Index() {
           </Link>
         </div>
       </section>
+
+      <section className="text-gray-600 body-font my-3">
+        <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <div className="flex flex-col items-center -m-4">
+            <div className="p-4 lg:w-1/2 md:w-full">
+              <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+                <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="#3d74ff"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-8 h-8"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                  </svg>
+                </div>
+                <div className="flex-grow">
+                  <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
+                    Courses
+                  </h2>
+                  <p className="leading-relaxed text-base">
+                    All the courses from BE(Hons) and BSc are available on our
+                    website. You can view the details, including prerequisites
+                    and summaries on the "Courses" page.
+                  </p>
+                  <Link
+                    href="/courses"
+                    className="mt-3 text-[#3d74ff] inline-flex items-center text-2xl font-semibold"
+                  >
+                    Go to Courses
+                    <svg
+                      fill="none"
+                      stroke="#3d74ff"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-7 h-7 ml-2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="p-4 lg:w-1/2 md:w-full">
+              <div className="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+                <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+                  <svg
+                    fill="none"
+                    stroke="#3d74ff"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    className="w-10 h-10"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                  </svg>
+                </div>
+                <div className="flex-grow">
+                  <h2 className="text-gray-900 text-lg title-font font-medium mb-3">
+                    Contact us
+                  </h2>
+                  <p className="leading-relaxed text-base">
+                    If you have any questions or messages, you can visit the
+                    contact page for more information.
+                  </p>
+                  <Link
+                    href="/contact"
+                    className="mt-3 text-[#3d74ff] inline-flex items-center text-2xl font-semibold"
+                  >
+                    Learn More
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-7 h-78 ml-2"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
