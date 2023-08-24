0cf4e913fee919ffb8ef061ab7635b3fb27e9fb3
Jason17401 <jason48.p@gmail.com>
2023-08-24 16:00:46 +1200

    feat: change login page format

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,65 +1,55 @@
-import Link from 'next/link'
-import Messages from './messages'
+import Messages from "./messages";
 
 export default function Login() {
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
+    <div className="flex justify-center items-center">
+      <div className="flex-1 flex flex-col w-full max-w-md px-8 py-8 gap-2">
+        <form
+          className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+          action="/auth/sign-in"
+          method="post"
         >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{' '}
-        Back
-      </Link>
-
-      <form
-        className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-        action="/auth/sign-in"
-        method="post"
-      >
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">
-          Sign In
-        </button>
-        <button
-          formAction="/auth/sign-up"
-          className="border border-gray-700 rounded px-4 py-2 text-black mb-2"
-        >
-          Sign Up
-        </button>
-        <Messages />
-      </form>
+          <label className="text-md" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            name="email"
+            placeholder="example@domain.com"
+            required
+          />
+          <label className="text-md" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            type="password"
+            name="password"
+            placeholder="••••••••"
+            required
+          />
+          <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">
+            Sign In
+          </button>
+          <button
+            formAction="/auth/sign-up"
+            className="border border-gray-700 rounded px-4 py-2 text-black mb-2"
+          >
+            Sign Up
+          </button>
+          <Messages />
+        </form>
+        <div className="flex-1 flex flex-col w-full items-center gap-2 text-foreground">
+          <p>or</p>
+        </div>
+        <div className="flex-1 flex flex-col w-full items-center gap-2 text-foreground">
+          {/* Obtained from https://tailwindflex.com/shakti/google-login-signup-button */}
+          <button className="px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
+            <img className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"/>
+            <span>Login with Google</span>
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
