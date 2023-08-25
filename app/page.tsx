5be6a1f1f3ad0c3489332eac90aeb889231ed67f
Chan Yoo <101070043+cyoo934@users.noreply.github.com>
2023-08-25 13:01:04 +1200

    Implemented Course API (#23)
    * Initialized course database
    
    Using UoA API referencing
    
    * Implemented course api
    
    Implemented course api, getting subject, title, catalog_number, has_requirement, academic_group
    
    * feat: allow query chaining in search
    
    ---------
    
    Co-authored-by: Raymond <65014987+rl16432@users.noreply.github.com>

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import LogoutButton from '../components/LogoutButton'
 import SupabaseLogo from '../components/SupabaseLogo'
 import NextJsLogo from '../components/NextJsLogo'
+import { initializeCourses } from '@/seed/SeedCourses'
 
 export const dynamic = 'force-dynamic'
 
