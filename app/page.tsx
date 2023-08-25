b2a9e6272b76122cd64f47c36bec6a41a777e7e8
rl16432 <rluo52237@gmail.com>
2023-08-25 13:24:59 +1200

    refactor: fix code smells

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import Link from 'next/link'
-import LogoutButton from '../components/LogoutButton'
-import SupabaseLogo from '../components/SupabaseLogo'
-import NextJsLogo from '../components/NextJsLogo'
-import { initializeCourses } from '@/seed/SeedCourses'
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
