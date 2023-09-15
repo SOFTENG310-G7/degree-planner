9bf6c1c3ca5bed90561c95fc5ae5a3ebbe93777c
joaomad247 <joao.madelino@gmail.com>
2023-09-16 00:51:01 +1200

    Fixing code smells

--- components/StarRating.tsx
+++ components/StarRating.tsx
@@ -9,12 +9,15 @@ const StarRating = () => {
     <div className="pt-10 ml-10">
       <div className="flex gap-2">
         {[...Array(5)].map((star, index) => {
+          const id = index;
           const ratingValue = index + 1;
+          const starColor = ratingValue <= (hover !== undefined ? hover : rating || 0) ? "#ffc107" : "#e4e5e9";
+          
           return (
             <FaStar
-              key={index}
+              key={id}
               className="star"
-              color={ratingValue <= (hover !== undefined ? hover : rating || 0) ? "#ffc107" : "#e4e5e9"}
+              color={starColor}
               size={30}
               cursor="pointer"
               onMouseEnter={() => setHover(ratingValue)}
