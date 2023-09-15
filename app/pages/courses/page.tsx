15a551f13aa1bdd5e5b22a902859fcb2c7404ecd
joaomad247 <joao.madelino@gmail.com>
2023-09-15 22:19:46 +1200

    Created Popup component

--- app/pages/courses/page.tsx
+++ app/pages/courses/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import CourseBlock from "@/components/CourseBlock";
+import Popup from "@/components/Popup";
 import { useEffect, useState } from "react";
 
 type Course = {
@@ -32,8 +33,10 @@ export default function Courses() {
 
   const closePopup = () => {
     setOpened(false);
+    document.body.style.overflow = ""; // Enable scrolling
   };
 
+  // Getting course data from database
   useEffect(() => {
     const fetchAPI = async () => {
       const fetchdata = await fetch("http://localhost:3000/api/courses");
@@ -55,34 +58,28 @@ export default function Courses() {
     setResults(filteredArr);
   }, [searchValue]);
 
+  // Controlling scrolling depending on if the popup is open
+  useEffect(() => {
+    if (opened) {
+      document.body.style.overflow = "hidden"; // Disable scrolling
+    } else {
+      document.body.style.overflow = ""; // Enable scrolling
+    }
+  }, [opened]);
+
   return (
     <div className="flex flex-col items-center">
+      {opened && openedData != null && (
+        // Making the background darker when the popup is open
+        <div
+          className="fixed left-0 right-0 top-0 bottom-0 bg-black opacity-50"
+          onClick={closePopup}
+        ></div>
+      )}
       <div>
-        {opened && openedData != null ? (
-          <div
-            id="popup"
-            className="fixed left-0 right-0 top-0 bottom-0 flex items-center justify-center w-2/3 m-auto"
-          >
-            <div className="border-2 border-grey-300 rounded-lg mx-[200px] my-[300px] bg-white">
-              <button className="text-right w-full p-2" onClick={closePopup}>
-                x
-              </button>
-              <div className="flex flex-col px-10 pb-10">
-                <div className="text-[26px] font-bold pt-10">
-                  {openedData.course_code}
-                </div>
-                <div className="text-[18px] italic text-[#3d3d3d] pb-2">
-                  {openedData.title}
-                </div>
-                <hr className="py-2" />
-                <div className="pb-10">{openedData.description}</div>
-                <div className="pb-10 italic text-grey-300 ">
-                  {openedData.requirement_description}
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : null}
+      {opened && openedData != null ? (
+        <Popup openedData={openedData} closePopup={closePopup} />
+      ) : null}
       </div>
       <div className="font-bold text-[40px] pt-[80px]">Courses</div>
       <div className=" w-1/2 border-2 border-grey-400 px-2 py-4 rounded-full my-[80px] flex">
