9c10d7fac266a062cddd88bc5f5d74a971537282
JACE <jacexin77@gmail.com>
2023-09-16 15:10:12 +1200

    removed div

--- app/pages/profile/page.tsx
+++ app/pages/profile/page.tsx
@@ -2,13 +2,11 @@ import LogoutButton from "@/components/LogoutButton";
 
 export default function Profile() {
   return (
-    <div>
-      <div className="flex flex-col justify-center items-center text-3xl">
-        <div className="font-bold w-1/5 text-center py-[200px]">
-          Welcome to your profile page.
-          <div className="mt-12">
-            <LogoutButton />
-          </div>
+    <div className="flex flex-col justify-center items-center text-3xl">
+      <div className="font-bold w-1/5 text-center py-[200px]">
+        Welcome to your profile page.
+        <div className="mt-12">
+          <LogoutButton />
         </div>
       </div>
     </div>
