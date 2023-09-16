24ad6f69fec64a66c9fbdab71a7cf42547e2a1d9
JACE <jacexin77@gmail.com>
2023-09-16 15:11:22 +1200

    added p tag around text

--- app/pages/profile/page.tsx
+++ app/pages/profile/page.tsx
@@ -4,7 +4,7 @@ export default function Profile() {
   return (
     <div className="flex flex-col justify-center items-center text-3xl">
       <div className="font-bold w-1/5 text-center py-[200px]">
-        Welcome to your profile page.
+        <p>Welcome to your profile page.</p>
         <div className="mt-12">
           <LogoutButton />
         </div>
