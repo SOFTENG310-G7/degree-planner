3ed62500d710a64659e7389a2b3196cb2adcfc5c
JACE <jacexin77@gmail.com>
2023-09-18 20:41:51 +1200

    make route for adding review

--- app/api/ratings/route.ts
+++ app/api/ratings/route.ts
@@ -1,16 +1,57 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
+import { parse } from "cookie";
 
 export const dynamic = "force-dynamic";
 
 export async function POST(request: Request) {
-  const supabase = createRouteHandlerClient(
-    { cookies },
-    { supabaseKey: process.env.SUPABASE_SERVICE_KEY }
-  );
+  try {
+    const supabase = createRouteHandlerClient(
+      { cookies },
+      { supabaseKey: process.env.SUPABASE_SERVICE_KEY }
+    );
 
-  const data = await request.json();
-  //TODO finish route
+    const data = await request.json();
 
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    console.log(user);
+
+    // Extract the user ID from the cookie (you may need to adjust this logic)
+    if (!user) {
+      console.error("User ID not found in the cookie.");
+      return new Response("User ID not found.", { status: 400 });
+    }
+
+    // Include the user ID in the data object
+    const { rating, courseId } = data;
+
+    // Insert the data into your Supabase table
+    const { data: insertedData, error } = await supabase
+      .from("reviews")
+      .upsert([
+        {
+          profile_id: user.id, // Include the user ID
+          rating: rating,
+          course_id: courseId,
+          // Add other properties as needed
+        },
+      ]);
+
+    if (error) {
+      console.error("Error inserting data into Supabase:", error);
+      return new Response("Error inserting data", { status: 500 });
+    }
+
+    console.log("Data inserted into Supabase:");
+
+    return new Response("Review added successfully", { status: 200 });
+  } catch (error) {
+    console.error("Error while handling POST request:", error);
+    return new Response("Error", { status: 500 });
+  }
 }
--- components/StarRating.tsx
+++ components/StarRating.tsx
@@ -9,23 +9,29 @@ const StarRating: React.FC<StarRatingProps> = ({ openedData }) => {
   const [rating, setRating] = useState<number | null>(null);
   const [hover, setHover] = useState<number | undefined>(undefined);
 
-  console.log(openedData.id);
-
+  const handleRatingSubmitWithDelay = async (ratingValue: React.SetStateAction<number | null>) => {
+    setRating(ratingValue);
+    console.log("1");
+    //wait 1 second
+    await new Promise((r) => setTimeout(r, 500));
+    console.log("2");
+    
+    Promise.resolve(handleRatingSubmit());
+    console.log("3");
+  };
+  
   const handleRatingSubmit = async () => {
     try {
-      // Replace with your actual cookie and route URL
-      const routeUrl = "/api/addReview"; // Update with your route URL
-
       // Check if a rating has been selected
       if (rating !== null) {
         // Create a data object with the required information
         const data = {
           rating: rating,
-          courseId: openedData.id, // Assuming you have access to courseId in openedData
+          courseId: openedData.id,
         };
 
         // Send a POST request to your route using fetch
-        const response = await fetch(routeUrl, {
+        const response = await fetch("http://localhost:3000/api/ratings", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -47,24 +53,27 @@ const StarRating: React.FC<StarRatingProps> = ({ openedData }) => {
     }
   };
 
-
   return (
     <div className="pt-10 ml-10">
       <div className="flex gap-2">
         {[...Array(5)].map((star, index) => {
           const id = index;
           const ratingValue = index + 1;
-          
+
           return (
             <FaStar
               key={id}
               className="star"
-              color={ratingValue <= (hover ?? (rating ?? 0)) ? "#ffc107" : "#e4e5e9"}
+              color={
+                ratingValue <= (hover ?? rating ?? 0) ? "#ffc107" : "#e4e5e9"
+              }
               size={30}
               cursor="pointer"
               onMouseEnter={() => setHover(ratingValue)}
               onMouseLeave={() => setHover(undefined)}
-              onClick={() => setRating(ratingValue)}
+              onClick={() => {
+                handleRatingSubmitWithDelay(ratingValue);
+              }}
             />
           );
         })}
