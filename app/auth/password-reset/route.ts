69db1f7d72f3cd192b5a9af3590f7d4268dd2b6c
cste303 <100255176+cste303@users.noreply.github.com>
2023-09-25 16:19:45 +1300

    feature/ Forgot Password
    Added forgot password functionality

---
+++ app/auth/password-reset/route.ts
@@ -0,0 +1,29 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(request: Request) {
+  const requestUrl = new URL(request.url);
+  const formData = await request.formData();
+  const password = String(formData.get("password"));
+  const supabase = createRouteHandlerClient({ cookies });
+
+  const { error } = await supabase.auth.updateUser({ password: password });
+
+  if (error) {
+    return NextResponse.redirect(
+      `${requestUrl.origin}/pages/resetPasswordSuccess?error=Invalid Password`,
+      {
+        // a 301 status is required to redirect from a POST to a GET route
+        status: 301,
+      }
+    );
+  }
+
+  return NextResponse.redirect(requestUrl.origin, {
+    // a 301 status is required to redirect from a POST to a GET route
+    status: 301,
+  });
+}
