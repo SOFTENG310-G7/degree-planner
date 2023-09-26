5e5031be2e1ad11a8fee4c4aab3f8d9d02a65a58
JACE <jacexin77@gmail.com>
2023-09-26 17:54:18 +1300

    updated css class name

--- assets/checkIcon.tsx
+++ assets/checkIcon.tsx
@@ -8,7 +8,7 @@ export default function checkIcon() {
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth="3"
-      className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4 icon-size"
+      className="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4 check-icon-size"
       viewBox="0 0 24 24"
     >
       <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
