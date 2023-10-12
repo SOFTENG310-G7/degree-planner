6fa4c1bd18a5dfe0af3097646dcfefa0b07254d1
JACE <jacexin77@gmail.com>
2023-10-13 12:53:39 +1300

    added comments to course popup

---
+++ components/Disqus.tsx
@@ -0,0 +1,21 @@
+import { DiscussionEmbed } from "disqus-react";
+import React from "react";
+
+interface Props {
+  openedData: {
+    course_code: string;
+  };
+}
+
+function DisqusComments({ openedData }: Props) {
+  console.log(openedData);
+  const disqusConfig = {
+    url: `http://localhost:3000/pages/courses/${openedData.course_code}`,
+    identifier: `course_${openedData.course_code}`,
+    title: openedData.course_code,
+  };
+
+  return <DiscussionEmbed shortname="degree-planner" config={disqusConfig} />;
+}
+
+export default DisqusComments;
