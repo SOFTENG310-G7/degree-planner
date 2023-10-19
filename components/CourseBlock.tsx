0f2604aaf29a5c01bab2f438cac761ee413408c7
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-19 20:48:22 +1300

    feat: darker course code colour

--- components/CourseBlock.tsx
+++ components/CourseBlock.tsx
@@ -1,7 +1,7 @@
 export default function CourseBlock(props: any) {
   return (
     <div className="border-2 border-slate-500 flex flex-col items-stretch rounded-lg py-4 px-6 w-full h-32 hover:cursor-pointer shadow hover:shadow-lg transition-shadow bg-white">
-      <h2 className="courseCode text-lg text-slate-700 font-semibold tracking-wide">
+      <h2 className="courseCode text-lg text-slate-800 font-semibold tracking-wide">
         {props.code}
       </h2>
       <p className="text-slate-900" id="courseName">
