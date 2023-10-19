599501ce3027b75ac072d946ce7d467fe6d09833
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-19 20:48:48 +1300

    feat: card colour

--- components/CourseBlock.tsx
+++ components/CourseBlock.tsx
@@ -1,6 +1,6 @@
 export default function CourseBlock(props: any) {
   return (
-    <div className="border-2 border-slate-500 flex flex-col items-stretch rounded-lg py-4 px-6 w-full h-32 hover:cursor-pointer shadow hover:shadow-lg transition-shadow bg-white">
+    <div className="border-2 border-slate-500 flex flex-col items-stretch rounded-lg py-4 px-6 w-full h-32 hover:cursor-pointer shadow hover:shadow-lg transition-shadow bg-slate-50">
       <h2 className="courseCode text-lg text-slate-800 font-semibold tracking-wide">
         {props.code}
       </h2>
