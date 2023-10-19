b6b7055a5829437044896d6ee5a65c7f861ed8db
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-19 21:12:45 +1300

    feat: reword about page

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -3,40 +3,34 @@ import '../../globals.css';
 export default async function About() {
   return (
     <div className="flex justify-center items-center px-8 py-8">
-      <div className="flex-1 flex flex-col w-full max-w-md-custom px-8 py-8 bg-white rounded-lg shadow-md">
+      <main className="flex-1 flex flex-col w-full max-w-md-custom px-8 py-8 bg-white rounded-lg shadow-md">
         <div className="text-center">
-          <h1 className="text-2xl font-bold">About Us</h1>
-          <br />
-          <p className="text-gray-600 font-bold">
+          <h1 className="text-4xl text-medium font-bold">About Degree Planner</h1>
+          <p className="text-slate-600 font-semibold my-4">
             Welcome to the University of Auckland Degree Planner.
+            <br />A place for secondary and tertiary students to find their path.
           </p>
-          <p className="text-gray-600 font-bold">
-            A place for secondary and tertiary students to find their path.
-          </p>
-          <br />
         </div>
-        <div className="mt-4">
-          <p className="text-gray-600 text-justify">
-            Starting in 2023, we are The G7s under the University of Auckland SOFTENG310 course who
-            were tasked to design a state of the art website for students to plan their future
+        <div className="my-8 text-gray-600">
+          <p className="mb-4">
+            In 2023, as part of the SOFTENG&nbsb;310 paper at Waipapa Taumata Rau (the University of
+            Auckland), the G7s were tasked with building website for students to plan their future
             education.
           </p>
-          <br />
-          <p className="text-gray-600 text-justify">
-            Our website features a complete list of courses available for Bachelor of Science and
-            Bachelor of Engineering at the university for potential and current University of
-            Auckland students to view and plan their degree or trial other potential degrees in
-            their interest.
+          <p className="mb-4">
+            Degree Planner features a complete list of courses available for the university’s
+            Bachelor of Science and Bachelor of Engineering programmes, supporting potential and
+            current students in their efforts to view and plan their degree, or map out alternative
+            degree plans.
           </p>
-          <br />
-          <p className="text-gray-600 text-justify">
-            Our website encourages students to create and share degree plans and rate courses based
-            on past experiences to allow everyone to embrace the culture at UoA. Students are also
-            able to view university entrance requirements and recommended plans for essential
-            courses to aspire for a specific occupation.
+          <p className="mb-4">
+            We encourage students to create and share degree plans, and rate courses based on past
+            experiences to share and foster the culture at Waipapa Taumata Rau. Students may also
+            view entrance requirements and recommended plans for essential courses for the career
+            path they are pursuing.
           </p>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
