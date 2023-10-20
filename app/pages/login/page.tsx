7b5c936d8887204f0b35234a496576d0a4501e53
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-20 14:38:08 +1300

    feat: login and sign-up styling

--- app/pages/login/page.tsx
+++ app/pages/login/page.tsx
@@ -21,40 +21,42 @@ export default function Login() {
               action="/auth/sign-in"
               method="post"
             >
-              <label className="text-md" htmlFor="email">
+              <label className="font-semibold text-slate-700" htmlFor="email">
                 Email
               </label>
               <input
                 id="email"
                 autoComplete="on"
-                className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                className="rounded-md px-4 py-2 bg-inherit border mb-6 focus:outline-cyan-600 focus-visible:outline-cyan-600 transition-colors"
                 name="email"
                 placeholder="example@domain.com"
                 required
               />
-              <label className="text-md" htmlFor="password">
+              <label className="font-semibold text-slate-700" htmlFor="password">
                 Password
               </label>
               <input
                 id="password"
-                className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                className="rounded-md px-4 py-2 bg-inherit border mb-6 focus-visible:outline-cyan-600 transition-colors"
                 type="password"
                 name="password"
                 placeholder="••••••••"
                 required
               />
-              <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">Sign In</button>
+              <button className="bg-green-700 hover:bg-green-600 focus:bg-green-760 focus-visible:bg-green-600 active:bg-green-700 focus:outline-none focus-visible:outline-none transition-colors rounded px-4 py-2 text-slate-50 font-medium mb-2">
+                Sign in
+              </button>
               <Messages />
             </form>
             <div className="flex-1 flex flex-row w-full justify-center items-center gap-2 text-foreground">
-              <span>Don't have an account?</span>
-              <button onClick={handleForm}>
-                <u>Sign Up</u>
+              <span>Don’t have an account?</span>
+              <button className="text-cyan-600 hover:underline" onClick={handleForm}>
+                Sign Up
               </button>
             </div>
             <div className="flex-1 flex flex-row w-full justify-center items-center gap-2 text-foreground">
-              <Link href="/pages/sendResetEmail">
-                <u>Forgot Password</u>
+              <Link className="text-cyan-600 hover:underline" href="/pages/sendResetEmail">
+                Forgot password?
               </Link>
             </div>
           </>
@@ -65,46 +67,48 @@ export default function Login() {
               action="/auth/sign-up"
               method="post"
             >
-              <label className="text-mid" htmlFor="username">
+              <label className="font-semibold text-slate-700" htmlFor="username">
                 Username
               </label>
               <input
                 id="username"
                 autoComplete="on"
-                className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                className="rounded-md px-4 py-2 bg-inherit border mb-6 focus:outline-cyan-600 focus-visible:outline-cyan-600 transition-colors"
                 name="username"
                 placeholder="bobby123"
                 required
               />
-              <label className="text-md" htmlFor="email">
+              <label className="font-semibold text-slate-700" htmlFor="email">
                 Email
               </label>
               <input
                 id="email"
                 autoComplete="on"
-                className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                className="rounded-md px-4 py-2 bg-inherit border mb-6 focus:outline-cyan-600 focus-visible:outline-cyan-600 transition-colors"
                 name="email"
                 placeholder="example@domain.com"
                 required
               />
-              <label className="text-md" htmlFor="password">
+              <label className="font-semibold text-slate-700" htmlFor="password">
                 Password
               </label>
               <input
                 id="password"
-                className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                className="rounded-md px-4 py-2 bg-inherit border mb-6 focus:outline-cyan-600 focus-visible:outline-cyan-600 transition-colors"
                 type="password"
                 name="password"
                 placeholder="••••••••"
                 required
               />
-              <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">Sign Up</button>
+              <button className="bg-green-700 hover:bg-green-600 focus:bg-green-760 focus-visible:bg-green-600 active:bg-green-700 focus:outline-none focus-visible:outline-none transition-colors rounded px-4 py-2 text-slate-50 mb-2">
+                Sign up
+              </button>
               <Messages />
             </form>
             <div className="flex-1 flex flex-row w-full justify-center items-center gap-2 text-foreground">
-              <span>Already a user?</span>
-              <button onClick={handleForm}>
-                <u>Sign In</u>
+              <p>Already a user?</p>
+              <button className="underline text-cyan-600 hover:underline" onClick={handleForm}>
+                Sign in
               </button>
             </div>
           </>
