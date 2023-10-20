57c0ecde9b73c05563951c934d28242a0095bef2
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-21 11:52:02 +1300

    feat: style logout button

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,8 +1,8 @@
 export default function LogoutButton() {
   return (
     <form action="/auth/sign-out" method="post">
-      <button className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-base px-4 py-2">
-        Logout
+      <button className="text-slate-50 bg-cyan-700 hover:bg-cyan-600 font-medium rounded-lg text-base px-4 py-2">
+        Log out
       </button>
     </form>
   );
