b857511fa3a38f05db07f4e277d431edb8930cbd
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-22 12:57:27 +1300

    refactor: navbar styling

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,33 +11,27 @@ export default async function NavBar() {
   } = await supabase.auth.getUser();
 
   return (
-    <div className="navbar">
-      <header className="text-slate-600 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 my-3 flex-col md:flex-row items-center">
-          <Link
-            href="/"
-            className="flex title-font font-medium items-center text-slate-900 mb-4 md:mb-0"
-          >
-            <span className="text-xl font-semibold">UoA Degree Planner</span>
-          </Link>
+    <header className="flex flex-row px-16 py-7 items-baseline justify-between text-slate-600 font-medium border-b-2 border-b-slate-300 tracking-wide shadow">
+      <Link
+        href="/"
+        className="flex text-xl font-semibold items-baseline text-cyan-900 traking-normal mb-4 md:mb-0"
+      >
+        Degree Planner
+      </Link>
 
-          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-slate-400 font-medium flex flex-row gap-4 items-center text-base justify-center [&>a]:px-1">
-            {/* TODO: Make navbar links a component and apply styling there */}
-            <Link href="/pages/courses">Courses</Link>
-            <Link href="/pages/planner">Degree Planner</Link>
-            <Link href="/pages/about">About</Link>
-            <Link href="/pages/contact">Contact</Link>
-          </nav>
-          <div className="font-medium">
-            {user ? (
-              <Link href="/pages/profile">{user.email}</Link>
-            ) : (
-              <Link href="/pages/login">Login</Link>
-            )}
-          </div>
-        </div>
-        <hr className="h-px bg-slate-300 border-0 dark:bg-slate-300 m-0" />
-      </header>
-    </div>
+      <nav className="flex flex-row gap-6 items-baseline text-base">
+        {/* TODO: Make navbar links a component and apply styling there */}
+        <Link href="/pages/courses">Courses</Link>
+        <Link href="/pages/planner">Degree Planner</Link>
+        <Link href="/pages/about">About</Link>
+        <Link href="/pages/contact">Contact</Link>
+      </nav>
+
+      {user ? (
+        <Link href="/pages/profile">{user.email}</Link>
+      ) : (
+        <Link href="/pages/login">Login</Link>
+      )}
+    </header>
   );
 }
