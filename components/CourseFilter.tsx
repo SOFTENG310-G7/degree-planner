8ba4bae0ec32f8e44dc503abb3b52b21e3397027
joaomad247 <joao.madelino@gmail.com>
2023-10-21 18:23:47 +1300

    feat: created filter for courses

---
+++ components/CourseFilter.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+type CourseFilterProps = {
+  handleCategoryChange: (category: string) => void;
+};
+
+const CourseFilter: React.FC<CourseFilterProps> = ({ handleCategoryChange }) => {
+  const [selectedCategory, SetSelectedCategory] = useState('All');
+
+  const handleSelectChange = (category: string) => {
+    SetSelectedCategory(category);
+    handleCategoryChange(category);
+  };
+
+    return (
+        <div className="w-1/2 mb-20">
+        <select
+          className="w-full border-2 border-slate-400 p-2 rounded-full"
+          value={selectedCategory}
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) => handleSelectChange(e.target.value)}
+        >
+          <option value="All">All</option>
+          <option value="CHEMMAT">Chemical and Materials Engineering</option>
+          <option value="ELECTENG">Electrical and Electronic Engineering</option>
+          <option value="SOFTENG">Software Engineering</option>
+          <option value="BIOMENG">Biomedical Engineering</option>
+          <option value="CIVIL">Civil Engineering</option>
+          <option value="COMPSYS">Computer Systems Engineering</option>
+          <option value="ENGSCI">Engineering Science</option>
+          <option value="MECHENG">Mechanical Engineering</option>
+          <option value="MECHENG">Mechatronics Engineering</option>
+          <option value="STRCTENG 200">Structural Engineering</option>
+        </select>
+      </div>
+    )
+}
+
+export default CourseFilter;
