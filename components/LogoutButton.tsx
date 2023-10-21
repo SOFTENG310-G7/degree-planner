b969746d7bef74e304f15d908a6b5278f9cdf1c1
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-22 12:21:50 +1300

    refactor: base styling for `<label>` & `<button>`

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,9 +1,7 @@
 export default function LogoutButton() {
   return (
     <form action="/auth/sign-out" method="post">
-      <button className="text-slate-50 bg-cyan-700 hover:bg-cyan-600 font-medium rounded-lg text-base px-4 py-2">
-        Log out
-      </button>
+      <button className="btn">Log out</button>
     </form>
   );
 }
