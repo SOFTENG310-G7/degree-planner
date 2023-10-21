d369db3d926879478614d02f98cef0c3471f894a
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-22 12:42:02 +1300

    reafactor: navbar link styling

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -18,32 +18,21 @@ export default async function NavBar() {
             href="/"
             className="flex title-font font-medium items-center text-slate-900 mb-4 md:mb-0"
           >
-            <span className="ml-3 text-xl font-semibold">UoA Degree Planner</span>
+            <span className="text-xl font-semibold">UoA Degree Planner</span>
           </Link>
 
           <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-slate-400 font-medium flex flex-row gap-4 items-center text-base justify-center [&>a]:px-1">
-            <Link href="/pages/courses" className="hover:text-cyan-600 transition-colors">
-              Courses
-            </Link>
-            <Link href="/pages/planner" className="hover:text-cyan-600 transition-colors">
-              Degree Planner
-            </Link>
-            <Link href="/pages/about" className="hover:text-cyan-600 transition-colors">
-              About
-            </Link>
-            <Link href="/pages/contact" className="hover:text-cyan-600 transition-colors">
-              Contact
-            </Link>
+            {/* TODO: Make navbar links a component and apply styling there */}
+            <Link href="/pages/courses">Courses</Link>
+            <Link href="/pages/planner">Degree Planner</Link>
+            <Link href="/pages/about">About</Link>
+            <Link href="/pages/contact">Contact</Link>
           </nav>
           <div className="font-medium">
             {user ? (
-              <Link href="/pages/profile" className="hover:text-cyan-600">
-                {user.email}
-              </Link>
+              <Link href="/pages/profile">{user.email}</Link>
             ) : (
-              <Link href="/pages/login" className="hover:text-cyan-600">
-                Login
-              </Link>
+              <Link href="/pages/login">Login</Link>
             )}
           </div>
         </div>
