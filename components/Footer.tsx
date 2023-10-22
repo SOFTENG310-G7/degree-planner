de118ef0346e830db521b72ff8e4662187bd3b32
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-22 14:28:26 +1300

    refactor: footer styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,25 +2,16 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="border-t border-gray-300 text-gray-600 body-font">
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <Link
-          href="/"
-          className="flex title-font font-medium items-center md:justify-start justify-center text-slate-900"
-        >
-          <p className="ml-3 text-xl font-bold">UoA Degree Planner</p>
-        </Link>
-        <p className="text-sm text-slate-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          ©&nbsp;2023 SOFTENG&nbsp;310 The G7
-        </p>
-        <a
-          href="https://www.auckland.ac.nz/"
-          target="_blank"
-          className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start hover:underline"
-        >
-          <p>The University of Auckland</p>
-        </a>
-      </div>
+    <footer className="flex flex-row justify-between items-baseline border-t border-slate-300 text-slate-600 px-16 py-7 tracking-wide min-h-[12rem]">
+      <Link href="/" className="flex text-xl font-semibold text-cyan-900 tracking-tight">
+        Degree Planner
+      </Link>
+      <p className="text-sm text-slate-500">
+        ©&nbsp;2023 SOFTENG&nbsp;310 Degree Planner Contributors
+      </p>
+      <a href="https://www.auckland.ac.nz" target="_blank">
+        University of Auckland
+      </a>
     </footer>
   );
 }
