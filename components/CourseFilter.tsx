751ff5d8fd69b4c58e470d117351e60280bc0876
joaomad247 <joao.madelino@gmail.com>
2023-10-22 13:52:43 +1300

    fix: fixing formatting

--- components/CourseFilter.tsx
+++ components/CourseFilter.tsx
@@ -13,22 +13,22 @@ const CourseFilter: React.FC<CourseFilterProps> = ({ handleCategoryChange }) => 
   };
 
   return (
-      <select
-        className="test w-1/2 mb-20 border-2 border-slate-400 px-4 py-2 rounded-full focus-within:border-cyan-600 transition-colors"
-        value={selectedCategory}
-        onChange={(e: React.ChangeEvent<HTMLSelectElement>) => handleSelectChange(e.target.value)}
-      >
-        <option value="All">All</option>
-        <option value="CHEMMAT">Chemical and Materials Engineering</option>
-        <option value="ELECTENG">Electrical and Electronic Engineering</option>
-        <option value="SOFTENG">Software Engineering</option>
-        <option value="BIOMENG">Biomedical Engineering</option>
-        <option value="CIVIL">Civil Engineering</option>
-        <option value="COMPSYS">Computer Systems Engineering</option>
-        <option value="ENGSCI">Engineering Science</option>
-        <option value="MECHENG">Mechanical Engineering</option>
-        <option value="STRCTENG">Structural Engineering</option>
-      </select>
+    <select
+      className="test w-1/2 mb-20 border-2 border-slate-400 px-4 py-2 rounded-full focus-within:border-cyan-600 transition-colors"
+      value={selectedCategory}
+      onChange={(e: React.ChangeEvent<HTMLSelectElement>) => handleSelectChange(e.target.value)}
+    >
+      <option value="All">All</option>
+      <option value="CHEMMAT">Chemical and Materials Engineering</option>
+      <option value="ELECTENG">Electrical and Electronic Engineering</option>
+      <option value="SOFTENG">Software Engineering</option>
+      <option value="BIOMENG">Biomedical Engineering</option>
+      <option value="CIVIL">Civil Engineering</option>
+      <option value="COMPSYS">Computer Systems Engineering</option>
+      <option value="ENGSCI">Engineering Science</option>
+      <option value="MECHENG">Mechanical Engineering</option>
+      <option value="STRCTENG">Structural Engineering</option>
+    </select>
   );
 };
 
