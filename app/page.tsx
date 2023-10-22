f8348ebd41ad189589df0f9c4a44b4d3ec85fe11
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-22 15:05:01 +1300

    refactor: eliminate redundant `<div>`

--- app/page.tsx
+++ app/page.tsx
@@ -35,48 +35,44 @@ export default async function Index() {
         </Link>
       </section>
 
-      <section className="text-slate-600 body-font my-3">
-        <div className="flex flex-col items-center px-5 py-24 -m-4">
-          <div className="p-4 lg:w-1/2 md:w-full">
-            <div className="flex border-2 rounded-lg border-slate-200 border-opacity-50 p-8 sm:flex-row flex-col">
-              <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-slate-100 text-cyan-600 flex-shrink-0">
-                {courseIcon()}
-              </div>
-              <div className="flex-grow">
-                <h2 className="text-slate-900 text-lg title-font font-medium mb-3">Courses</h2>
-                <p className="leading-relaxed text-base">
-                  All the courses from BE(Hons) and BSc are available on our website. You can view
-                  the details, including prerequisites and summaries on the Courses page.
-                </p>
-                <Link
-                  href="/pages/courses"
-                  className="mt-3 text-cyan-600 inline-flex items-center text-2xl font-semibold"
-                >
-                  See courses
-                  {arrowIcon()}
-                </Link>
-              </div>
+      <section className="text-slate-600 my-3">
+        <div className="flex flex-col gap-6 items-center px-5 py-24">
+          <div className="lg:w-1/2 md:w-full flex border-2 rounded-lg border-slate-200 border-opacity-50 p-8 sm:flex-row flex-col">
+            <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-slate-100 text-cyan-600 flex-shrink-0">
+              {courseIcon()}
+            </div>
+            <div className="flex-grow">
+              <h2 className="text-slate-900 text-lg title-font font-medium mb-3">Courses</h2>
+              <p className="leading-relaxed text-base">
+                All the courses from BE(Hons) and BSc are available on our website. You can view the
+                details, including prerequisites and summaries on the Courses page.
+              </p>
+              <Link
+                href="/pages/courses"
+                className="mt-3 text-cyan-600 inline-flex items-center text-2xl font-semibold"
+              >
+                See courses
+                {arrowIcon()}
+              </Link>
             </div>
           </div>
-          <div className="p-4 lg:w-1/2 md:w-full">
-            <div className="flex border-2 rounded-lg border-slate-200 border-opacity-50 p-8 sm:flex-row flex-col">
-              <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-slate-100 text-cyan-600 flex-shrink-0">
-                {contactIcon()}
-              </div>
-              <div className="flex-grow">
-                <h2 className="text-slate-900 text-lg title-font font-medium mb-3">Contact us</h2>
-                <p className="leading-relaxed text-base">
-                  If you have any questions or messages, you can visit the contact page for more
-                  information.
-                </p>
-                <Link
-                  href="/pages/contact"
-                  className="mt-3 text-cyan-600 inline-flex items-center text-2xl font-semibold"
-                >
-                  Learn more
-                  {arrowIcon()}
-                </Link>
-              </div>
+          <div className="lg:w-1/2 md:w-full flex border-2 rounded-lg border-slate-200 border-opacity-50 p-8 sm:flex-row flex-col">
+            <div className="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-slate-100 text-cyan-600 flex-shrink-0">
+              {contactIcon()}
+            </div>
+            <div className="flex-grow">
+              <h2 className="text-slate-900 text-lg title-font font-medium mb-3">Contact us</h2>
+              <p className="leading-relaxed text-base">
+                If you have any questions or messages, you can visit the contact page for more
+                information.
+              </p>
+              <Link
+                href="/pages/contact"
+                className="mt-3 text-cyan-600 inline-flex items-center text-2xl font-semibold"
+              >
+                Learn more
+                {arrowIcon()}
+              </Link>
             </div>
           </div>
         </div>
