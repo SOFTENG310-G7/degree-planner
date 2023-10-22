cbbd0f12adad627146b49f4bee0d6d7044d2f97d
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2023-10-22 22:44:00 +1300

    feat: GitHub link in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,15 +3,24 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="flex flex-row flex-wrap justify-between items-baseline border-t border-slate-300 text-slate-600 px-16 py-7 tracking-wide min-h-[12rem]">
-      <Link href="/" className="flex text-xl font-semibold text-cyan-900 tracking-tight">
-        Degree Planner
-      </Link>
+      <div>
+        <Link href="/" className="flex text-xl font-semibold text-cyan-900 tracking-tight">
+          Degree Planner
+        </Link>
+        <Link
+          href="https://www.auckland.ac.nz"
+          className="font-medium text-slate-500"
+          target="_blank"
+        >
+          University of Auckland
+        </Link>
+      </div>
       <p className="text-sm text-slate-500">
         ©&nbsp;2023 SOFTENG&nbsp;310 Degree Planner Contributors
       </p>
-      <a href="https://www.auckland.ac.nz" target="_blank">
-        University of Auckland
-      </a>
+      <Link href="https://github.com/SOFTENG310-G7/degree-planner" target="_blank">
+        GitHub
+      </Link>
     </footer>
   );
 }
